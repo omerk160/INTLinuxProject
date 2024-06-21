581a912f7e1a07a387297092465c1b05595a0a71
omerk160 <omerkanias160@gmail.com>
2024-06-21 14:15:41 +0300

    linux project final!

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,19 +1,21 @@
-cd /home/omer/
+
 wget https://alonitac.github.io/DevOpsTheHardWay/linux_project/secretGenerator.tar.gz
-tar -xvf /home/omer/secretGenerator.tar.gz
-cd /home/omer/src
-chmod +x /home/omer/src/generateSecret.sh
-mkdir /home/omer/src/secretDir
+tar -xvf secretGenerator.tar.gz
+cd src
+chmod +x generateSecret.sh
+mkdir secretDir
 echo "directory created"
-rm -rf /home/omer/src/maliciousFiles
+rm -rf maliciousFiles
 echo "removed maclicous"
-touch /home/omer/src/secretDir/.secret
+cd secretDir
+touch .secret
 echo "created .secret file"
-chmod 600 /home/omer/src/secretDir/.secret
+chmod 600 .secret
 echo "given permissions to the file"
-rm /home/omer/src/important.link
+cd ..
+rm important.link
 echo "removed the link"
-/bin/bash /home/omer/src/generateSecret.sh
+./generateSecret.sh
 #cat home/omer/src/secretDir/.secret && echo "there is the hash"
 
 
